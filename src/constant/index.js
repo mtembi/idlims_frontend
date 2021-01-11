e7ca6b42a4c51a7c9f119d2e9c29ecc17a1827c5
Mtembi <mtembi@gmail.com>
2021-01-11 19:29:23 +0300

    Allow logins

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,5 +1,5 @@
-//export const API_URL="https://idlims-be.herokuapp.com";
-export const API_URL="http://localhost:5000";
+export const API_URL="https://idlims-be.herokuapp.com";
+//export const API_URL="http://localhost:5000";
 export const API_HEADERS={
     headers:{
         "Authorization": localStorage.getItem('user'),
